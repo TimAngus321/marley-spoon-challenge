6587caf5f5b4f0daafe84048cefdfad643daa181
Timothy Angus <timangus321@gmail.com>
2023-09-01 20:33:24 +0200

    quick cleanup

--- src/pages/RecipeHomePage/recipeHome.tsx
+++ src/pages/RecipeHomePage/recipeHome.tsx
@@ -13,18 +13,6 @@ const RecipeHome = () => {
       <header>
         <h1>Marley Spoon Recipes</h1>
       </header>
-      {/* {recipesObj ?
-      Object.values(recipesObj).map((recipe, i) => (
-      <RecipeListGrid 
-        key={i}
-        title={recipe?.title}
-        image={recipe?.imageUrl} 
-        tags={recipe?.tags} 
-        desc={recipe?.description}
-        chefName={recipe?.chefName}
-       />
-       ))
-       : null} */}
        <RecipeListGrid props={recipesObj} />
     </main>
   );
